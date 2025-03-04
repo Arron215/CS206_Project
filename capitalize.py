0ded3abdddcc13f8309064d8c98712d790de6dbf
Arron215 <72841843+Arron215@users.noreply.github.com>
2025-03-04 14:52:24 -0800

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,7 +1,16 @@
-#Toy function from Homework 1, capitalizes an inputted string
+#Toy function largely from Homework 1, capitalizes an inputted string
+#Adding more error checking to handle additional cases that might not produce expected output
 
 def capitalize(sentence: str) -> str:
   from string import ascii_lowercase, ascii_uppercase
   
   if not sentence:
   return ""
+
+  #check if we are trying to capitalize a number
+  if 
+  
+  lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
+  return lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:]
+
+test_set = []
