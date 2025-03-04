8ee43628645a11ccb678003a193903fb1733bc9c
Arron215 <72841843+Arron215@users.noreply.github.com>
2025-03-04 15:04:15 -0800

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -13,4 +13,19 @@ def capitalize(sentence: str) -> str:
   lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
   return lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:]
 
+def capitalize_no_string(sentence: str) -> none:
+  from string import ascii_lowercase, ascii_uppercase
+
+  sentence -> (str)
+  
+  if not sentence:
+  return ""
+
+  #check if we are trying to capitalize a number
+  if 
+  
+  lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
+  return lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:]
+
 test_set = []
+capitalize_no_string(1hi298)
