0534c50ac39242768fd6fe49f632ae4e4de24ce4
Arron215 <72841843+Arron215@users.noreply.github.com>
2025-03-05 00:11:22 +0000

    Light edit to capitalize

--- capitalize.py
+++ capitalize.py
@@ -5,27 +5,25 @@ def capitalize(sentence: str) -> str:
   from string import ascii_lowercase, ascii_uppercase
   
   if not sentence:
-  return ""
+    return ""
 
   #check if we are trying to capitalize a number
-  if 
+  #if 
   
   lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
-  return lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:]
+  return print(lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:])
 
 def capitalize_no_string(sentence):
   from string import ascii_lowercase, ascii_uppercase
-
-  sentence -> (str)
   
   if not sentence:
-  return ""
+    return ""
 
   #check if we are trying to capitalize a number
-  if 
+  #if 
   
   lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
-  return lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:]
+  return print(lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:])
 
 test_set = []
-capitalize_no_string(1hi298)
+capitalize_no_string("1hi298")
