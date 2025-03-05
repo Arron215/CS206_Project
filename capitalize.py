629f025c031f76f2fa7d03c6fe659d4f9d881a84
Arron215 <72841843+Arron215@users.noreply.github.com>
2025-03-04 21:26:02 -0800

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -25,5 +25,12 @@ def capitalize_no_string(sentence):
   lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
   return print(lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:])
 
-test_set = []
-capitalize_no_string("1hi298")
+import random
+
+test_set_success = ["i am a cherry pie", "happy and i know it", "where is the candy", "can i have some", "abcde", "efgh", "poppyseed", ascii_lowercase, ascii_uppercase]
+test_set_fail = ["12345a", "$avc", "", ",abc", ".baec", " "]
+test_set = ([test_set_success, test_set_fail], k=(sizeof(test_set_success)+sizeof(test_set_fail)))
+
+for i in test_set
+  print(test_set[i])
+  
