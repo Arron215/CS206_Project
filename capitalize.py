03d6ee0541c66024c62a0f44e3be4b600ca7c2a4
Arron215 <72841843+Arron215@users.noreply.github.com>
2025-03-06 11:38:05 -0800

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -42,19 +42,19 @@ def capitalize_no_string(sentence):
   lower_to_upper = dict(zip(ascii_lowercase, ascii_uppercase))
   return print(lower_to_upper.get(sentence[0], sentence[0]) + sentence[1:])
 
-import random
-import sys
-import string
-from string import ascii_lowercase, ascii_uppercase
-
-test_set_success = ["i am a cherry pie", "happy and i know it", "where is the candy", "can i have some", "abcde", "efgh", "poppyseed"]
-test_set_fail = ["12345a", "$avc", "", ",abc", ".baec", " "]
-
-test_set = test_set_success
-test_set.extend(test_set_fail)
-test_set.extend(string.printable)
-print(test_set)
-print("\n")
+def test_set_cap
+  import random
+  import sys
+  import string
+  from string import ascii_lowercase, ascii_uppercase
+  
+  test_set_success = ["i am a cherry pie", "happy and i know it", "where is the candy", "can i have some", "abcde", "efgh", "poppyseed"]
+  test_set_fail = ["12345a", "$avc", "", ",abc", ".baec", " "]
+  
+  test_set = test_set_success
+  test_set.extend(test_set_fail)
+  test_set.extend(string.printable)
+  return test_set
 
 random.shuffle(test_set)
 print(test_set)
